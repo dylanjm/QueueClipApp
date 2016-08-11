9a5277fa97bc9c15a91f74231a093d5f03510ca9
Dylan McDowell <dylanmcdowell@dylans-macbook-pro.local>
2016-08-11 14:50:42 -0600

    Initial Commit

---
+++ QueueClipApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  QueueClipApp
+//
+//  Created by Dylan McDowell on 8/11/16.
+//  Copyright © 2016 Dylan McDowell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
